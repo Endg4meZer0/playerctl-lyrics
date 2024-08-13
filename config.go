9c11068fccd66f40f6d0efd1c49a947feee14e2b
Endg4meZer0 <timka.tarasen@gmail.com>
2024-08-13 17:58:05 +1000

    feat: more config structs

--- config.go
+++ config.go
@@ -2,20 +2,36 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"os"
 )
 
+// LEVEL 0
+
 type Config struct {
 	Playerctl    Playerctl    `json:"playerctl"`
+	Cache        Cache        `json:"cache"`
 	Output       Output       `json:"output"`
 	Instrumental Instrumental `json:"instrumental"`
 }
 
+// LEVEL 1
+
 type Playerctl struct {
-	ExcludedPlayers            []string `json:"excludedPlayers"`
 	IncludedPlayers            []string `json:"includedPlayers"`
+	ExcludedPlayers            []string `json:"excludedPlayers"`
 	PlayerctlSongCheckInterval float64  `json:"playerctlMetadataCheckInterval"`
+}
+
+type Cache struct {
+	DoCacheLyrics bool   `json:"doCacheLyrics"`
+	CacheDir      string `json:"cacheDir"`
+	CacheLifeSpan uint   `json:"cacheLifeSpan"`
+}
+
+type Output struct {
+	ShowSongNotFoundWarning    bool         `json:"showSongNotFoundWarning"`
+	ShowNotSyncedLyricsWarning bool         `json:"showNotSyncedLyricsWarning"`
+	Romanization               Romanization `json:"romanization"`
 }
 
 type Instrumental struct {
@@ -24,29 +40,44 @@ type Instrumental struct {
 	MaxCount uint    `json:"maxCount"`
 }
 
-type Output struct {
-	ShowSongNotFoundWarning bool `json:"showSongNotFoundWarning"`
-	MaximumCharacterLength  uint `json:"maximumCharacterLength"`
-	ScrollText              bool `json:"scrollText"`
+// LEVEL 2
+
+type Romanization struct {
+	Japanese bool `json:"japanese"`
+	Chinese  bool `json:"chinese"`
+	Korean   bool `json:"korean"`
 }
 
-func ReadConfig(path string) Config {
+var currentConfig Config
+
+func GetConfig() Config {
+	return currentConfig
+}
+
+func ReadConfig(path string) error {
 	jsonConfig, err := os.ReadFile(path)
 	if err != nil {
-		log.Printf("The config at %v does not exist or is not readable! Falling back to default config!", path)
+		return err
 	}
 
-	var config Config
-	if json.Unmarshal(jsonConfig, &config) != nil {
-		log.Printf("The config at %v does not exist or is not readable! Falling back to default config!", path)
+	if err := json.Unmarshal(jsonConfig, &currentConfig); err != nil {
+		return err
+		// "The config at %v does not exist or is not readable! Falling back to the default config!"
+		// "The config at %v is not formatted as JSON! Falling back to the default config!"
+		// "The config at %v is not valid! Error at %v. Falling back to the default config!"
 	}
-	return config
+
+	if err := currentConfig.validateConfig(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func ValidateConfig() []error {
+func (config *Config) validateConfig() error {
 	panic("not implemented!!1")
 }
 
-func DefaultConfig() Config {
+func defaultConfig() Config {
 	panic("not implemented!!1")
 }
