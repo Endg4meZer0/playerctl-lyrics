0fd82f92698699f14d06d47ae9e60cfd99b5e10c
Endg4me_ <timka.tarasen@gmail.com>
2024-11-17 00:30:56 +1000

    feat(lyrics): now removes "\r" at the end of lines if met any feat(lyrics): now handles repeated timestamps on one line properly

--- internal/lyrics/dto/lrclib/dto.go
+++ internal/lyrics/dto/lrclib/dto.go
@@ -2,7 +2,7 @@ package lrclib
 
 import (
 	"lrcsnc/internal/pkg/structs"
-	"lrcsnc/internal/pkg/util"
+	"strconv"
 	"strings"
 )
 
@@ -29,29 +29,68 @@ func (dto LrcLibDTO) ToLyricsData() (out structs.LyricsData) {
 	}
 
 	out.LyricsType = 0
-	syncedLyrics := strings.Split(dto.SyncedLyrics, "\n")
-
-	resultLyrics := make([]string, len(syncedLyrics))
-	resultTimestamps := make([]float64, len(syncedLyrics))
-
-	for i, lyric := range syncedLyrics {
-		lyricParts := strings.SplitN(lyric, " ", 2)
-		timecode := util.TimecodeToFloat(lyricParts[0])
-		if timecode == -1 {
-			continue
-		}
-		var lyricStr string
-		if len(lyricParts) != 1 {
-			lyricStr = lyricParts[1]
-		} else {
-			lyricStr = ""
-		}
-		resultLyrics[i] = lyricStr
-		resultTimestamps[i] = timecode
-	}
+	resultLyrics, resultTimestamps := parseSyncedLyrics(dto.SyncedLyrics)
 
 	out.Lyrics = resultLyrics
 	out.LyricTimestamps = resultTimestamps
 
 	return
+}
+
+func parseSyncedLyrics(lyrics string) (out []string, timestamps []float64) {
+	unsorted := false
+	syncedLyrics := strings.Split(lyrics, "\n")
+
+	resultLyrics := make([]string, 0, len(syncedLyrics))
+	resultTimestamps := make([]float64, 0, len(syncedLyrics))
+
+	for _, lyric := range syncedLyrics {
+		lyricParts := strings.Split(lyric, "]")
+		if len(lyricParts) < 2 {
+			continue
+		}
+
+		lyricStr := strings.TrimRight(strings.TrimSpace(lyricParts[len(lyricParts)-1]), "\r")
+		unsorted = unsorted || len(lyricParts) > 2
+
+		for _, timecodeStr := range lyricParts[:len(lyricParts)-1] {
+			timecode := timecodeToFloat(timecodeStr)
+			if timecode == -1 {
+				continue
+			}
+			resultLyrics = append(resultLyrics, lyricStr)
+			resultTimestamps = append(resultTimestamps, timecode)
+		}
+	}
+
+	if unsorted {
+		// Sort the timestamps and lyrics
+		// Not really a lot of data, and the "repeating" format of lyrics appears very rarely, so using bubble sort is not going to impact general performance
+		for i := 0; i < len(resultTimestamps); i++ {
+			for j := i + 1; j < len(resultTimestamps); j++ {
+				if resultTimestamps[i] > resultTimestamps[j] {
+					resultTimestamps[i], resultTimestamps[j] = resultTimestamps[j], resultTimestamps[i]
+					resultLyrics[i], resultLyrics[j] = resultLyrics[j], resultLyrics[i]
+				}
+			}
+		}
+	}
+
+	return resultLyrics, resultTimestamps
+}
+
+func timecodeToFloat(timecode string) float64 {
+	// [01:23.45
+	if len(timecode) != 9 {
+		return -1
+	}
+	minutes, err := strconv.ParseFloat(timecode[1:3], 64)
+	if err != nil {
+		return -1
+	}
+	seconds, err := strconv.ParseFloat(timecode[4:9], 64)
+	if err != nil {
+		return -1
+	}
+	return minutes*60.0 + seconds
 }
